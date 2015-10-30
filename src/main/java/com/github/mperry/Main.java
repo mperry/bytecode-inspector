4435e04f8c029ddbbdbf6e51a1cc58eb03756a69
Mark Perry <maperry78@yahoo.com.au>
2015-10-30 22:24:16 +1000

    Added application plugin

---
+++ src/main/java/com/github/mperry/Main.java
@@ -0,0 +1,15 @@
+package com.github.mperry;
+
+/**
+ * Created by MarkPerry on 10/30/2015.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("hi world");
+    }
+
+
+
+
+}
